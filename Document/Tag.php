cb9f3262eb906d7ba8023f90fe9dcd7b55fff1c6
Gerry Vandermaesen <gerryvdm@me.com>
2015-07-22 19:39:42 +0200

    Add link tag implementation

---
+++ Document/Tag.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ShareMonkey\Document;
+
+final class Tag
+{
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * @param string $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
